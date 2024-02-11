e53d005fa35fc96dc4c516468ae62a49e4bb0112
Mathieu Briau <mathieu.briau@gmail.com>
2024-02-11 18:26:06 -0500

    Solution du component

--- ngPollingEtSignalR/src/app/signalr/signalr.component.ts
+++ ngPollingEtSignalR/src/app/signalr/signalr.component.ts
@@ -21,16 +21,42 @@ export class SignalrComponent implements OnInit {
 
   connecttohub() {
     // TODO On doit commencer par créer la connexion vers le Hub
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7289/tasks')
+      .build();
+    // TODO On se connecte au Hub
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('La connexion est live!');
+        // TODO Une fois connectée, on peut commencer à écouter pour les évènements qui vont déclencher des callbacks
+        this.hubConnection!.on('UserCount', (data) => {
+          this.usercount = data;
+        });
 
-    // TODO Une fois connectée, on peut commencer à écouter pour les évènements qui vont déclencher des callbacks
+        this.hubConnection!.on('TaskList', (data) => {
+          console.log(data);
+          this.tasks = data;
+        });
+      })
+      .catch(err => console.log('Error while starting connection: ' + err))
   }
+
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
+    this.hubConnection!.invoke('CompleteTask', id);
   }
 
   addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
+    this.hubConnection!.invoke('AddTask', this.taskname);
   }
 
-}
+
+});
+
+
+
+//this.hubConnection!.invoke('ConnectNewUser')
+
