3c82022b3aeb17b6a75a8723a6b7718b0a93e08c
CMandziuk (superjekk) <superjekk@outlook.com>
2024-09-13 10:14:37 -0400

    FCT : Communication en direct par rafraîchissement

--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UselessTask } from '../models/UselessTask';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-polling',
@@ -19,16 +20,22 @@ export class PollingComponent implements OnInit {
     this.updateTasks();
   }
 
-  complete(id: number) {
+  async complete(id: number) {
+    await lastValueFrom(this.http.get("https://localhost:7289/api/UselessTasks/Complete/" + id));
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
   }
 
-  addtask() {
+  async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    await lastValueFrom(this.http.post("https://localhost:7289/api/UselessTasks/Add?taskText=" + this.taskname, {}));
   }
 
-  updateTasks() {
+  async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    let resultat = await lastValueFrom(this.http.get<UselessTask[]>("https://localhost:7289/api/UselessTasks/GetAll"));
+    this.tasks = resultat;
+
+    setTimeout(() => this.updateTasks(), 500);
   }
 }
